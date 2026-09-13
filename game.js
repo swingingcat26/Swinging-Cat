dec6a7af14259776c119876c1b526ffb7d9c28a1
swingingcat26 <swingingcat26@gmail.com>
2026-09-13 10:56:27 +0200

    fix

--- game.js
+++ game.js
@@ -168,20 +168,20 @@ document.getElementById('ranking2').addEventListener('click', async () => {
         // 🟢 AGGIUNTA SBARRA IN FONDO
         if (myPos) {
             let footerText = (myPos <= 250)
-                ? `Your position: ${myPos}°  <span style="font-weight: 700; color: #2575fc; margin-left: 10px; font-family: 'Nunito', sans-serif;">${myScore} pt</span>`
-                : `You are better than ${(((totalUsers - myPos) / totalUsers) * 100).toFixed(1)}% of players`;
+                ? `<label style="font-family: 'Nunito', sans-serif;">Your position: ${myPos}°  <span style="font-weight: 700; color: #2575fc; margin-left: 10px; font-family: 'Nunito', sans-serif;">${myScore} pt</span></label>`
+                : `<label style="font-family: 'Nunito', sans-serif;">You are better than ${(((totalUsers - myPos) / totalUsers) * 100).toFixed(1)}% of players</label>`;
 
             htmlClassifica += `
-                <div style="border-top: 2px solid #e2e8f0; padding: 16px auto; margin-top: 8px; font-weight: bold; text-align: center; background: #f8fafc; border-radius: 12px; position: sticky; bottom: -4px;">
+                <div style=" padding: 16px auto; margin-top: 8px; font-weight: bold; text-align: center; background: #f8fafc; border-radius: 12px; position: sticky; bottom: -4px;">
                     ${footerText}
                 </div>`;
         };
 
         if (!auth.currentUser || auth.currentUser.isAnonymous) { 
-                    let ctaRanks = `Register to join the ranks <span style="font-weight: 700; color: #2575fc; margin-left: 10px; font-family: 'Nunito', sans-serif;">${highScore} pt</span>`;
+                    let ctaRanks = `<label style="font-family: 'Nunito', sans-serif;">Register to join the ranks <span style="font-weight: 700; color: #2575fc; margin-left: 10px; font-family: 'Nunito', sans-serif;">${highScore} pt</span></label>`;
 
 htmlClassifica += `
-    <div style="border-top: 2px solid #e2e8f0; padding: 16px auto; margin-top: 8px; font-weight: bold; text-align: center; background: #f8fafc; border-radius: 12px; position: sticky; bottom: -4px;">
+    <div style="padding: 16px auto; margin-top: 8px; font-weight: bold; text-align: center; background: #f8fafc; border-radius: 12px; position: sticky; bottom: -4px;">
         ${ctaRanks}
     <button id="btnLogin" style="background: black; color: white; width: auto; height: auto; padding: 4px 8px; font-size: 18px; font-weight: 700; margin-top: 4px;">Log In</button>
     </div>`;
